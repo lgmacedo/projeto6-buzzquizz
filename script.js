296d4e358db28abce8aa3d3647d0e3290fd0fd41
lgmacedo <lucasguerra.macedo@gmail.com>
2023-01-25 14:35:30 -0300

    style finished for all questions and answers; static

--- script.js
+++ script.js
@@ -4,20 +4,63 @@
 
  
 //  screen number two - play the quiz - outset // 
-function iniciaPagina2(){
+function startScreen2(){
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/1");
-    promise.then(sucessoPag2);
-    promise.catch(erroPag2);
+    promise.then(successScreen2);
+    promise.catch(errorScreen2);
 }
 
-function sucessoPag2(dados){
+function successScreen2(dados){
     console.log("DEU CERTO!!!");
     console.log(dados);
-    document.querySelector('.screennumbertwo .img-title p').innerHTML = dados.data.title;
-    document.querySelector('.screennumbertwo .img-title img').src = dados.data.image;
+    const quizScreen = document.querySelector('.screennumbertwo')
+
+    quizScreen.innerHTML += 
+    `<div class="img-title">
+        <img src="${dados.data.image}">
+        <p>${dados.data.title}</p>
+    </div>`;
+
+    const arrQuestions = dados.data.questions;
+    for(let i = 0; i<arrQuestions.length; i++){
+        const arrAnswers = arrQuestions[i].answers;
+        for (let i = arrAnswers.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let aux = arrAnswers[i];
+            arrAnswers[i] = arrAnswers[j];
+            arrAnswers[j] = aux;
+        }
+
+        quizScreen.innerHTML +=
+        `<div class="question">
+            <div class="question-title">
+                <p>${arrQuestions[i].title}</p>
+            </div>
+            <div class="answer-container">
+                <div class="answer">
+                    <img src="${arrAnswers[0].image}">
+                    <p>${arrAnswers[0].text}</p>
+                </div>
+                <div class="answer">
+                    <img src="${arrAnswers[1].image}">
+                    <p>${arrAnswers[1].text}</p>
+                </div>
+                <div class="answer">
+                    <img src="${arrAnswers[2].image}">
+                    <p>${arrAnswers[2].text}</p>
+                </div>
+                <div class="answer">
+                    <img src="${arrAnswers[3].image}">
+                    <p>${arrAnswers[3].text}</p>
+                </div>
+            </div>
+        </div>`;
+
+        document.querySelector(`.question:last-child .question-title`).style.backgroundColor = arrQuestions[i].color;
+    }
 }
 
-function erroPag2(dados){
+function errorScreen2(dados){
     console.log("DEU ERRADO!!!");
     console.log(dados);
 }
